2265210a320886377367b2867a6eb2558cede50c
Terry <122493619+TerryYuxiang@users.noreply.github.com>
2024-02-23 19:16:55 -0800

    Lose game

---
+++ Big_Hero_6_Dev/Assets/Main/Scripts/LoseGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseGame : MonoBehaviour
+{
+    public float pauseThreshold = -10f;
+
+    public GameObject loseGameObject;
+
+    void Update()
+    {
+        if (transform.position.y < pauseThreshold)
+        {
+            Time.timeScale = 0;
+            loseGameObject.SetActive(true);
+        }
+    }
+}
