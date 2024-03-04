7fc049267ca487d0bd916cc26d10e43c9441e58f
illidanwu <illidanwoo@gmail.com>
2024-03-03 21:02:03 -0800

    Modified Level 1 and Added Unity Analytics
    Modified Level 1 and Added Unity Analytics

--- Big_Hero_6_Dev/Assets/Main/Scripts/PauseGameOnEnter.cs
+++ Big_Hero_6_Dev/Assets/Main/Scripts/PauseGameOnEnter.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement; 
 public class PauseGameOnEnter : MonoBehaviour
 {
     public GameObject winGameObject;
 
+	void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        Debug.Log("Current scene is : " + currentScene.name);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") && Global.yellowKey && Global.redKey)
         {
-            Debug.Log("Game Pause");
+            Debug.Log("Game Over. You Suscceed!");
             Time.timeScale = 0;
             winGameObject.SetActive(true);
         }
--- Big_Hero_6_Dev/Assets/Main/Tutorial_Scripts/PauseGameOnEnter.cs
+++ Big_Hero_6_Dev/Assets/Main/Tutorial_Scripts/PauseGameOnEnter.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement; 
 public class PauseGameOnEnter_Tutorial : MonoBehaviour
 {
     public GameObject winGameObject;
-
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        
+        Debug.Log("Current Scene is: " + currentScene.name);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Game Pause");
+            Debug.Log("Game Over. You Suscceed!");
             Time.timeScale = 0;
             winGameObject.SetActive(true);
         }
