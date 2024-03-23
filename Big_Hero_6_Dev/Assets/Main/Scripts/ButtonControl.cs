e50cb647beca0e82a3f1bc1d1ceeeb0e29804b53
mingmao110 <mingmao@usc.edu>
2024-03-22 22:44:17 -0700

    with next level

--- Big_Hero_6_Dev/Assets/Main/Scripts/ButtonControl.cs
+++ Big_Hero_6_Dev/Assets/Main/Scripts/ButtonControl.cs
@@ -7,42 +7,91 @@ public class ButtonControl : MonoBehaviour
 {
     public void MainMenu()
     {
-        Global.redKey = false;
-        Global.yellowKey = false;
-        Time.timeScale = 1;
+        ResetGame();
         SceneManager.LoadScene("Main Menu");
     }
 
     public void Level1()
     {
-        Global.redKey = false;
-        Global.yellowKey = false;
-        Time.timeScale = 1;
+        ResetGame();
         SceneManager.LoadScene("Level 1");
     }
 
     public void Level2()
     {
-        Global.redKey = false;
-        Global.yellowKey = false;
-        Time.timeScale = 1;
+        ResetGame();
         SceneManager.LoadScene("Level 2");
     }
 
-    public void Tutorial1()
+    public void Level3()
     {
-        Global.redKey = false;
-        Global.yellowKey = false;
-        Time.timeScale = 1;
-        SceneManager.LoadScene("Tutorial1");
+        ResetGame();
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void Level4()
+    {
+        ResetGame();
+        SceneManager.LoadScene("Level 4");
+    }
+
+    public void Level5()
+    {
+        ResetGame();
+        SceneManager.LoadScene("Level 5");
+    }
+
+    public void Level6()
+    {
+        ResetGame();
+        SceneManager.LoadScene("Level 6");
     }
 
 
+
+    public void Tutorial1()
+    {
+        ResetGame();
+        SceneManager.LoadScene("Tutorial1");
+    }
+
     public void Tutorial2()
+    {
+        ResetGame();
+        SceneManager.LoadScene("Tutorial2");
+    }
+
+    public void NextLevel()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        switch (currentScene)
+        {
+            case "Main Menu":
+                Tutorial1();
+                break;
+            case "Tutorial1":
+                Tutorial2();
+                break;
+            case "Tutorial2":
+                Level1();
+                break;
+            case "Level 1":
+                Level2();
+                break;
+            case "Level 2":
+                MainMenu();
+                break;
+            default:
+                Debug.Log("Unrecognized scene or no next scene available.");
+                break;
+        }
+    }
+
+    private void ResetGame()
     {
         Global.redKey = false;
         Global.yellowKey = false;
         Time.timeScale = 1;
-        SceneManager.LoadScene("Tutorial2");
     }
 }
