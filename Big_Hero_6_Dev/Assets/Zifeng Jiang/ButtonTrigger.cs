2d6ba0be1d8f7e526304ba8bb8503ed662df34ee
Zifeng Jiang <zifengji@usc.edu>
2024-03-25 03:47:28 -0700

    part5

---
+++ Big_Hero_6_Dev/Assets/Zifeng Jiang/ButtonTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface ITriggerable
+{
+    void Trigger();
+}
+
+
+public class ButtonTrigger : MonoBehaviour
+{
+    public GameObject target;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision Detected");
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player has collided with the button");
+            ITriggerable triggerableComponent = target.GetComponent<ITriggerable>();
+            if (triggerableComponent != null)
+            {
+                triggerableComponent.Trigger();
+            }
+        }
+    }
+}
---
+++ Big_Hero_6_Dev/Assets/Zifeng Jiang/Door2Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door2Controller : MonoBehaviour, ITriggerable
+{
+    public float moveDistance = 1f; // 移动距离
+    public float moveSpeed = 2f; // 移动速度
+    private Vector3 originalPosition; // 原始位置
+    private Vector3 targetPosition; // 目标位置
+    private bool isOpening = false; // 是否正在打开
+
+    void Start()
+    {
+        originalPosition = transform.position; // 保存原始位置
+        targetPosition = originalPosition + new Vector3(moveDistance, 0, 0); // 计算目标位置
+    }
+
+    void Update()
+    {
+        if (isOpening)
+        {
+            // 移动门到目标位置
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Trigger()
+    {
+        isOpening = true; // 触发打开门
+    }
+}
