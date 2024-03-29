a17cbdddc03590e78de666eebf344be61d9a818a
mingmao110 <mingmao@usc.edu>
2024-03-29 01:59:21 -0700

    with esc menu

---
+++ Big_Hero_6_Dev/Assets/Ming Mao/MenuController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menu;
+
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menu.SetActive(!menu.activeSelf);
+            PauseGame(menu.activeSelf);
+        }
+    }
+
+    void PauseGame(bool isPaused)
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        menu.SetActive(false);
+        PauseGame(false);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadMainMenu()
+    {
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
