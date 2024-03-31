91fb26213ed8741ebe075d3e80e97b42c8fdc22d
Terry <122493619+TerryYuxiang@users.noreply.github.com>
2024-03-31 14:22:48 -0700

    level 2 traps fixed

--- Big_Hero_6_Dev/Assets/Main/Scripts/YellowKey.cs
+++ Big_Hero_6_Dev/Assets/Main/Scripts/YellowKey.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class YellowKey : MonoBehaviour
 {
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        Scene currentScene = SceneManager.GetActiveScene();
+
         if (collision.gameObject.CompareTag("Player"))
         {
             Global.yellowKey = true;
@@ -14,5 +19,16 @@ public class YellowKey : MonoBehaviour
 
             Debug.Log("Yellow key has been collected!");
         }
+
+        if (currentScene.name == "Level 2")
+        {
+            Debug.Log("Level two yellow key!");
+            ThronController thronController = FindObjectOfType<ThronController>();
+            if (thronController != null)
+            {
+                thronController.isOpening = true;
+            }
+        }
+
     }
 }
