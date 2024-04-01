2ee902f23abd28d2ce68fb4824e2ba28fc0c7f3f
Shanpeng Wang <shanpeng@usc.edu>
2024-03-31 22:02:28 -0700

    Public levelindex and add the position of shadowing
    Public levelindex and add the position of shadowing

--- Big_Hero_6_Dev/Assets/Main/Scripts/LevelAnalyticsManager.cs
+++ Big_Hero_6_Dev/Assets/Main/Scripts/LevelAnalyticsManager.cs
@@ -15,19 +15,26 @@ public class FinishLevelTime
     public string eventName;
     public double time;
     public long timeStamp;
+    //public double posX_shadowing;
+    //public double posY_shadowing;
 }
 public class LevelAnalyticsManager : MonoBehaviour
 {
     private float levelStartTime;
-    private string levelIndex;
+    //private string levelIndex;
+    public string LevelIndex;
+    //private double posX_shadowing;
+    //private double posY_shadowing;
+    //private double posX_TimeGel;
+    //private double posY_TimeGel;
 
     void Start()
     {
         string pattern = @"\d+";
 
         MatchCollection matches = Regex.Matches(SceneManager.GetActiveScene().name, pattern);
-        levelIndex = matches[matches.Count - 1].Value;
-        Debug.Log(levelIndex);
+        LevelIndex = matches[matches.Count - 1].Value;
+        Debug.Log(LevelIndex);
         StartLevel();
     }
 
@@ -35,6 +42,9 @@ public class LevelAnalyticsManager : MonoBehaviour
     {
         // Level Start
         levelStartTime = Time.time;
+        // Reset shadowing positions
+        //posX_shadowing = 0;
+        //posY_shadowing = 0;
         Debug.Log("Start Time: " + levelStartTime);
     }
 
@@ -46,22 +56,32 @@ public class LevelAnalyticsManager : MonoBehaviour
         long timestampSeconds = (long)timeSpan.TotalSeconds;
         return timestampSeconds;
     }
-
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.J))
+    //    {
+    //        var player_Position = transform.position;
+    //        posX_shadowing = player_Position.x;
+    //        posY_shadowing = player_Position.y;
+    //    }
+    //}
     public void EndLevel()
     {
         // Level End
         float levelEndTime = Time.time;
         Debug.Log("End Time: " + levelEndTime);
         float timeToComplete = levelEndTime - levelStartTime;
-        string levelTimeEvent = "FinishLevel" + levelIndex;
-        Debug.Log(levelIndex);
-        if (levelIndex == "1")
+        string levelTimeEvent = "FinishLevel" + LevelIndex;
+        Debug.Log(LevelIndex);
+        if (LevelIndex == "1")
         {
             FinishLevelTime finishLevelTime = new FinishLevelTime();
             finishLevelTime.level = "Level1";
             finishLevelTime.eventName = "Level1_FinishTime";
             finishLevelTime.time = timeToComplete;
             finishLevelTime.timeStamp = GetTimeStamp();
+            //finishLevelTime.posX_shadowing = posX_shadowing;
+            //finishLevelTime.posY_shadowing= posY_shadowing;
             string json = JsonUtility.ToJson(finishLevelTime);
             RestClient.Post("https://big-hero-6-1efc3-default-rtdb.firebaseio.com/.json", finishLevelTime);
             /*CustomEvent FinishLevel1 = new CustomEvent(levelTimeEvent)
@@ -72,13 +92,15 @@ public class LevelAnalyticsManager : MonoBehaviour
             };
             AnalyticsService.Instance.RecordEvent(FinishLevel1);*/
         }
-        else if (levelIndex == "2")
+        else if (LevelIndex == "2")
         {
             FinishLevelTime finishLevelTime = new FinishLevelTime();
             finishLevelTime.level = "Level2";
             finishLevelTime.eventName = "Level2_FinishTime";
             finishLevelTime.time = timeToComplete;
             finishLevelTime.timeStamp = GetTimeStamp();
+            //finishLevelTime.posX_shadowing = posX_shadowing;
+            //finishLevelTime.posY_shadowing = posY_shadowing;
             string json = JsonUtility.ToJson(finishLevelTime);
             RestClient.Post("https://big-hero-6-1efc3-default-rtdb.firebaseio.com/.json", finishLevelTime);
             /*CustomEvent FinishLevel2 = new CustomEvent(levelTimeEvent)
@@ -89,13 +111,15 @@ public class LevelAnalyticsManager : MonoBehaviour
             };
             AnalyticsService.Instance.RecordEvent(FinishLevel2);*/
         }
-        else if (levelIndex == "3")
+        else if (LevelIndex == "3")
         {
             FinishLevelTime finishLevelTime = new FinishLevelTime();
             finishLevelTime.level = "Level3";
             finishLevelTime.eventName = "Level3_FinishTime";
             finishLevelTime.time = timeToComplete;
             finishLevelTime.timeStamp = GetTimeStamp();
+            //finishLevelTime.posX_shadowing = posX_shadowing;
+            //finishLevelTime.posY_shadowing = posY_shadowing;
             string json = JsonUtility.ToJson(finishLevelTime);
             RestClient.Post("https://big-hero-6-1efc3-default-rtdb.firebaseio.com/.json", finishLevelTime);
         }
--- Big_Hero_6_Dev/Assets/Main/Scripts/Shadowing.cs
+++ Big_Hero_6_Dev/Assets/Main/Scripts/Shadowing.cs
@@ -1,7 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Proyecto26;
+public class PlayerPositon
+{
+    public string level;
+    public string eventName;
+    public double posX_Shadowing;
+    public double posY_Shadowing;
+}
 public class Shadowing : MonoBehaviour
 {
     public GameObject shadowPrefab; // Shadow Prefab
@@ -14,9 +21,13 @@ public class Shadowing : MonoBehaviour
     private int Moveindex;
     public int gelFlag = 0;
     public float shadowIndex = 1.2f;
+    private LevelAnalyticsManager levelAnalyticsManager;
+    private double posX_shadowing;
+    private double posY_shadowing;
     void Start()
     {
         shadowInstance = Instantiate(shadowPrefab, transform.position, Quaternion.identity);
+        levelAnalyticsManager = FindObjectOfType<LevelAnalyticsManager>();
     }
 
     void Update()
@@ -66,10 +77,46 @@ public class Shadowing : MonoBehaviour
         {
             //Moveindex = Mathf.Max(0, positionList.Count - Mathf.FloorToInt(shadowDelay / recordInterval)); //Get shadow's position
             //transform.position = positionList[Moveindex];
+            var player_Position = transform.position;
             transform.position = shadowInstance.transform.position;
             positionList.Clear(); // Clear old shadow's position
             
             positionList.Add(transform.position); // Restart the shddow
+            posX_shadowing = player_Position.x;
+            posY_shadowing = player_Position.y;
+            string currentLevelIndex = levelAnalyticsManager != null ? levelAnalyticsManager.LevelIndex : "Unknown";
+            string levelTimeEvent = "FinishLevel" + currentLevelIndex;
+            Debug.Log(currentLevelIndex);
+            if (currentLevelIndex == "1")
+            {
+                PlayerPositon playerPositon = new PlayerPositon();
+                playerPositon.level = "Level1";
+                playerPositon.eventName = "Level1_PlayerPositon_Shadowing";
+                playerPositon.posX_Shadowing = posX_shadowing;
+                playerPositon.posY_Shadowing = posY_shadowing;
+                string json = JsonUtility.ToJson(playerPositon);
+                RestClient.Post("https://big-hero-6-1efc3-default-rtdb.firebaseio.com/.json", playerPositon);
+            }
+            else if (currentLevelIndex == "2")
+            {
+                PlayerPositon playerPositon = new PlayerPositon();
+                playerPositon.level = "Level1";
+                playerPositon.eventName = "Level2_PlayerPositon_Shadowing";
+                playerPositon.posX_Shadowing = posX_shadowing;
+                playerPositon.posY_Shadowing = posY_shadowing;
+                string json = JsonUtility.ToJson(playerPositon);
+                RestClient.Post("https://big-hero-6-1efc3-default-rtdb.firebaseio.com/.json", playerPositon);
+            }
+            else if (currentLevelIndex == "3")
+            {
+                PlayerPositon playerPositon = new PlayerPositon();
+                playerPositon.level = "Level3";
+                playerPositon.eventName = "Level3_PlayerPositon_Shadowing";
+                playerPositon.posX_Shadowing = posX_shadowing;
+                playerPositon.posY_Shadowing = posY_shadowing;
+                string json = JsonUtility.ToJson(playerPositon);
+                RestClient.Post("https://big-hero-6-1efc3-default-rtdb.firebaseio.com/.json", playerPositon);
+            }
         }
     }
 }
