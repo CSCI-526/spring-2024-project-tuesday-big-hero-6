1d280fdb0be407090f0becb7f077ed323c78176f
mingmao110 <mingmao@usc.edu>
2024-04-12 15:38:46 -0700

    checkpoint color change

--- Big_Hero_6_Dev/Assets/Ming Mao/Checkpoint.cs
+++ Big_Hero_6_Dev/Assets/Ming Mao/Checkpoint.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using System.Collections;
 
 public class Checkpoint : MonoBehaviour
 {
+    public SpriteRenderer CheckpointIndicator;
+
+
+    private void Start()
+    {
+        CheckpointIndicator.color = Color.yellow;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            CheckpointIndicator.color = Color.green;
             SaveManager.Instance.UpdateCheckpoint(transform.position);
         }
     }
